10507101eb2a9bc0cbe950cc8642f27c58d379f5
Artem <a.khimin@yandex.ru>
2021-05-10 16:59:58 +0300

    Добавьте файлы проекта.

---
+++ CS095VirtualOverride/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS095VirtualOverride
+{
+    class Car
+    {
+        protected virtual void StartEngine()
+        {
+            Console.WriteLine("Двигатель запущен!");
+        }
+
+        public virtual void Drive()
+        {
+            StartEngine();
+
+            Console.WriteLine("Я машина, я еду!");
+        }
+    }
+
+    class SportCar:Car
+    {
+        protected override void StartEngine()
+        {
+            Console.WriteLine("др-др-дРРР-РРРР-рррр");
+        }
+
+        public override void Drive()
+        {
+            StartEngine();
+            Console.WriteLine("Я супер быстрая машина и я еду!");
+        }
+    }
+
+
+    class Person
+    {
+        public  void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            Person person = new Person();
+
+            person.Drive(car);
+
+            person.Drive(new SportCar());
+
+            Console.WriteLine("****************************");
+
+            person.Drive(new Car());
+
+        }
+    }
+}
